41e1d16cd6ee7ff231d86f0993e46e71199c4f74
Radosław WIeczorek <r.p.wieczorek@gmail.com>
2025-10-22 07:48:08 +0200

    FIx issues according to current requirments

---
+++ js/script.js
@@ -0,0 +1,32 @@
+  var swiper = new Swiper('#es-rc #es-garnier .swiper', {
+    // Optional parameters
+    // direction: 'horizontal',
+    loop: true,
+    autoHeight: false,
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '#es-rc #es-garnier .swiper-button-next',
+      prevEl: '#es-rc #es-garnier .swiper-button-prev'
+    },
+    pagination: {
+      el: '#es-rc #es-garnier .swiper-pagination',
+      clickable: true
+    }
+  });
+  var swiper = new Swiper('#es-rc #es-garnier .swiper-cross', {
+    // Optional parameters
+    // direction: 'horizontal',
+    loop: true,
+    autoHeight: false,
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '#es-rc #es-garnier .swiper-cross-button-next',
+      prevEl: '#es-rc #es-garnier .swiper-cross-button-prev'
+    },
+    pagination: {
+      el: '#es-rc #es-garnier .swiper-cross-pagination',
+      clickable: true
+    }
+  });
